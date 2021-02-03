53bd178327b6355212cf8b1edb79c57b8f132f74
Dan Ramos <dan.ramos-dominko@turner.com>
2021-02-03 11:17:04 -0700

    updates to seo config

--- server/sitemap.js
+++ server/sitemap.js
@@ -61,7 +61,7 @@ exports.sitemapStructure = () => {
       '/': ['get'],
       '/signup': ['get'],
       '/login': ['get'],
-      '/s?address=Helsinki%2C%20Finland%26bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056%26country=FI%26origin=60.16985569999999%2C24.93837910000002': [
+      '/s?address=United%20States%20of%20America&bounds=54.35813096%2C-96.59798738%2C26.93142286%2C-124.85162638&mapSearch=true': [
         'get',
       ],
       '/account': ['get'],
@@ -84,7 +84,7 @@ exports.sitemapStructure = () => {
         changefreq: 'always',
         priority: 1.0,
       },
-      '/s?address=Helsinki%2C%20Finland%26bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056%26country=FI%26origin=60.16985569999999%2C24.93837910000002': {
+      '/s?address=United%20States%20of%20America&bounds=54.35813096%2C-96.59798738%2C26.93142286%2C-124.85162638&mapSearch=true': {
         changefreq: 'always',
         priority: 1.0,
       },
--- src/config.js
+++ src/config.js
@@ -81,10 +81,10 @@ const sentryDsn = process.env.REACT_APP_SENTRY_DSN;
 const usingSSL = process.env.REACT_APP_SHARETRIBE_USING_SSL === 'true';
 
 // Address information is used in SEO schema for Organization (http://schema.org/PostalAddress)
-const addressCountry = 'FI';
-const addressRegion = 'Helsinki';
-const postalCode = '00100';
-const streetAddress = 'Bulevardi 14';
+const addressCountry = 'US';
+const addressRegion = 'Salt Lake City';
+const postalCode = '84044';
+const streetAddress = 'Highland Dr';
 
 // Canonical root url is needed in social media sharing and SEO optimization purposes.
 const canonicalRootURL = process.env.REACT_APP_CANONICAL_ROOT_URL;
